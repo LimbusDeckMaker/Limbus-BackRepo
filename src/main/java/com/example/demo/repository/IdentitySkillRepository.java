d403be8dcecea6c6c5a47f4ab7508459fd2af135
KimJM <ehxhfl1589@gmail.com>
2024-01-12 08:26:35 +0900

    complete all sync4 DB Input

--- src/main/java/com/example/demo/repository/IdentitySkillRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.domain.IdentitySkill;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IdentitySkillRepository extends JpaRepository<IdentitySkill, Long> {
-}
