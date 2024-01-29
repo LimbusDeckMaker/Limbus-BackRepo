16383d3cfd8e511569b43aa4f46dc023e1641b0c
KimJM <ehxhfl1589@gmail.com>
2024-01-30 04:03:29 +0900

    limbus company 최신 추출 API

--- src/main/java/com/example/limbusDeckMaker/controller/MainPageController.java
+++ src/main/java/com/example/limbusDeckMaker/controller/MainPageController.java
@@ -1,7 +1,10 @@
 package com.example.limbusDeckMaker.controller;
 
+import com.example.limbusDeckMaker.dto.steam.SteamNewsDto;
+import com.example.limbusDeckMaker.service.SteamService;
 import com.example.limbusDeckMaker.service.YouTubeService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.limbusDeckMaker.util.Views;
+import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +18,11 @@ import java.security.GeneralSecurityException;
 public class MainPageController {
 
     private final YouTubeService youTubeService;
+    private final SteamService steamService;
 
-    public MainPageController(YouTubeService youTubeService) {
+    public MainPageController(YouTubeService youTubeService, SteamService steamService) {
         this.youTubeService = youTubeService;
+        this.steamService = steamService;
     }
 
     @GetMapping("/youtube")
@@ -31,6 +36,26 @@ public class MainPageController {
         } catch (GeneralSecurityException | IOException e) {
             e.printStackTrace();
             return ResponseEntity.status(500).body("An error occurred.");
+        }
+    }
+
+    @JsonView(Views.Public.class)
+    @GetMapping("/contents")
+    public ResponseEntity<SteamNewsDto> getSteamNewsContent() {
+        try {
+            String titleKeyword = "New Target Extraction";
+            String contentKeyword = "{STEAM_CLAN_IMAGE}";
+
+            SteamNewsDto steamNewsContent = steamService.getNewsContent(titleKeyword, contentKeyword);
+
+            if (steamNewsContent != null && steamNewsContent.getUrl() != null) {
+                return ResponseEntity.ok(steamNewsContent);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body(null);
         }
     }
 }
